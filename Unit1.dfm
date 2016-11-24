825b9360c00988c2062a2e7b46a5618fd2f214f0
IgorMalyga <igormalyga@gmail.com>
2016-11-24 21:38:54 +0200

    second commit

--- Unit1.dfm
+++ Unit1.dfm
@@ -1,8 +1,8 @@
 object Form1: TForm1
-  Left = 199
-  Top = 117
-  Width = 1127
-  Height = 578
+  Left = 438
+  Top = 163
+  Width = 928
+  Height = 480
   Caption = 'Form1'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -11,51 +11,35 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
-  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
-  object StringGrid1: TStringGrid
-    Left = 64
-    Top = 24
-    Width = 977
-    Height = 457
-    ColCount = 15
+  object SG1: TStringGrid
+    Left = 16
+    Top = 16
+    Width = 889
+    Height = 177
+    ColCount = 13
     FixedCols = 0
-    RowCount = 2
+    RowCount = 1
     FixedRows = 0
-    Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goRangeSelect, goEditing]
     TabOrder = 0
   end
-  object Button1: TButton
-    Left = 264
-    Top = 488
-    Width = 177
-    Height = 33
-    Caption = #1057#1075#1077#1085#1088#1080#1088#1086#1074#1072#1090#1100' '#1089#1083#1091#1095#1072#1081#1085#1099#1077' '#1095#1080#1089#1083#1072
-    TabOrder = 1
-    OnClick = Button1Click
-  end
-  object Button2: TButton
-    Left = 448
-    Top = 488
-    Width = 177
+  object btn1: TButton
+    Left = 32
+    Top = 200
+    Width = 193
     Height = 33
     Caption = #1053#1072#1095#1072#1090#1100' '#1089#1086#1088#1090#1080#1088#1086#1074#1082#1091
-    TabOrder = 2
-    OnClick = Button2Click
+    TabOrder = 1
+    OnClick = btn1Click
   end
-  object Button3: TButton
-    Left = 632
-    Top = 488
-    Width = 177
+  object btn2: TButton
+    Left = 232
+    Top = 200
+    Width = 193
     Height = 33
-    Caption = #1058#1077#1089#1090
-    TabOrder = 3
-    OnClick = Button3Click
-  end
-  object Timer1: TTimer
-    OnTimer = Timer1Timer
-    Left = 816
-    Top = 488
+    Caption = #1057#1075#1077#1085#1080#1088#1080#1088#1086#1074#1072#1090#1100' '#1095#1080#1089#1083#1072
+    TabOrder = 2
+    OnClick = btn2Click
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,131 +4,106 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ExtCtrls, StdCtrls, Grids;
+  Dialogs, StdCtrls, Grids;
 
 type
+  Tarr = array of Integer;
   TForm1 = class(TForm)
-    StringGrid1: TStringGrid;
-    Button1: TButton;
-    Button2: TButton;
-    Button3: TButton;
-    Timer1: TTimer;
-    procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
-    procedure Button3Click(Sender: TObject);
-    procedure Timer1Timer(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
+    SG1: TStringGrid;
+    btn1: TButton;
+    btn2: TButton;
     //procedure mastOSTRINGgrid(A: Tarr; st:TstringGrid);
+    procedure ArrayToStringGrid(mas : Tarr; SG1 : TStringGrid);
+    procedure btn2Click(Sender: TObject);
+    procedure btn1Click(Sender: TObject);
+    //procedure TForm1.ArrayToStringGrid(var mas : Tarr; SG1 : TStringGrid);
+
+
   private
     { Private declarations }
   public
     { Public declarations }
   end;
 
+
 var
   Form1: TForm1;
-   i,l,r,middleindex,k : Integer;
-  sss : double;
+  mas : Tarr;
+
+
 
 implementation
+function InitArray(mas : Tarr; SG1 : TStringGrid) : Tarr;
+var i : Integer;
+begin
+ Randomize;
+ SetLength(mas,0);
+ for i:=0 to SG1.ColCount-1 do
+  begin
+    SetLength(mas,Length(mas) + 1);
+    mas[i] := Random(20);
+  end;
+  Result := mas;
+end;
+
+procedure TForm1.ArrayToStringGrid(mas : Tarr; SG1 : TStringGrid);
+var i,j : Integer;
+begin
+ for i:= 0 to High(mas) do
+ begin
+  SG1.Cells[i,0] := IntToStr(mas[i]);
+ end;
+end;
+
+procedure Swap(var mas : Tarr; i,middleindex : integer);
+var tmp : Integer;
+begin
+ tmp:=mas[i];
+ mas[i]:=mas[middleindex];
+ mas[middleindex]:=tmp;
+end;
+
+Function Partition(var mas : Tarr; l,r : integer) : Integer;
+var i,j,x : Integer;
+begin
+ x:=mas[r];
+ i:=l-1;
+ for j:=l to r-1 do
+  begin
+   if mas[j]<= x then
+    begin
+     i:=i+1;
+     Swap(mas,i,j);
+    end;
+  end;
+ Swap(mas,i+1,r);
+ result:=i+1;
+end;
+
+procedure QuickSort(mas : Tarr; l,r : integer);
+var q:Integer;
+begin
+ if l<r then
+  begin
+   q:=Partition(mas,l,r);
+   QuickSort(mas,l,q-1);
+   QuickSort(mas,q+1,r);
+  end;
+end;
 
 {$R *.dfm}
 
-function GetMiddleIndex(var StringGrid1  : TStringGrid) : Double;
-     var k  : Double;
-      begin
-        k:=StringGrid1.ColCount/2;
-        if StringGrid1.ColCount mod 2 <> 0 then
-          begin
-            k:=k+0.5;
-            Result:=k;
-          end
-          else
-          begin
-            result:=k;
-          end;
-
-
-      end;
-
-procedure Swap(var StringGrid1 : TStringGrid; i,middleindex : integer);
-        var tmp : String;
-        begin
-          tmp:=StringGrid1.Cells[i,0];
-          StringGrid1.Cells[i,0]:=StringGrid1.Cells[middleindex,0];
-          StringGrid1.Cells[middleindex,0]:=tmp;
-        end;
-
- //procedure TForm1.mastOSTRINGgrid(A: Tarr; st:TstringGrid);
- //begin
-
- //end;
-
- function Partition(var StringGrid1 : TStringGrid; l,r : integer) : Integer;
-        var i,j,x : Integer;
-        begin
-          x:=StrToInt(StringGrid1.Cells[r,0]);
-          i:=l-1;
-          for j:=l to r-1 do
-           begin
-             if StrToInt(StringGrid1.Cells[j,0])<= x then
-              begin
-                  i:=i+1;
-                  Swap(StringGrid1,i,j);
-                 // mastOSTRINGgrid(arr, stringGrid);
-                  Sleep(1000);
-              end;
-
-           end;
-           Swap(StringGrid1,i+1,r);
-           result:=i+1;
-
-        end;
-
-procedure QuickSort(StringGrid1 : TStringGrid; l,r : integer);
-          var q:Integer;
-          begin
-            if l<r then
-              begin
-                q:=Partition(StringGrid1,l,r);
-                QuickSort(StringGrid1,l,q-1);
-                //StringGrid1.RowCount:= StringGrid1.RowCount +1 ;
-                QuickSort(StringGrid1,q+1,r);
-              end;
-          end;
-
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TForm1.btn2Click(Sender: TObject);
 begin
- for i := 0 to StringGrid1.ColCount-1 do
- begin
-   StringGrid1.Cells[i,0]:=IntToStr(Random(100))
- end;
- l:=0; r:=StringGrid1.ColCount-1;
+Randomize;
+mas := InitArray(mas,SG1);
+ArrayToStringGrid(mas,SG1);
 end;
 
-procedure TForm1.Button2Click(Sender: TObject);
+procedure TForm1.btn1Click(Sender: TObject);
 begin
-QuickSort(StringGrid1,l,StringGrid1.ColCount-1);
-end;
-
-procedure TForm1.Button3Click(Sender: TObject);
-begin
-k:=1;
- Timer1.Enabled:=True;
-end;
-
-procedure TForm1.Timer1Timer(Sender: TObject);
-begin
-  
-if k>=StringGrid1.ColCount-2 then Timer1.Enabled:=False;
-   k:=k+1;
-   StringGrid1.Cells[k,0]:=StringGrid1.Cells[k-1,0];
-   StringGrid1.Cells[k-1,0]:=' ';
-end;
-
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-Timer1.Enabled:=False;
+QuickSort(mas,0,High(mas));
+ArrayToStringGrid(mas,SG1);
 end;
 
 end.
